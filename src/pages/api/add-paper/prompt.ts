26ab1a6eeeb23f6ddb5fb49177249b9b6e796437
Mikołaj Jakóbiak <jakobiakmikolaj@gmail.com>
2023-03-31 23:14:47 +0100

    filter openAI completion to get rid of unnecessary keys

--- src/pages/api/add-paper/prompt.ts
+++ src/pages/api/add-paper/prompt.ts
@@ -22,6 +22,31 @@ type AnswerDto = {
   pages?: { from: number; to: number }
 }
 
+const exampleAnswer: AnswerDto = {
+  type: 'Journal Article',
+  title: 'Some Title',
+  authors: [
+    {
+      fName: 'John',
+      lName: 'Doe',
+    },
+    {
+      fName: 'Jane',
+      lName: 'Doe',
+    },
+  ],
+  abstract: 'This is an abstract. It is a short summary of the article.',
+  year: 2015,
+  month: 1,
+  day: undefined,
+  publisher: 'Some Publisher',
+  publication: 'Name of a Journal',
+  doi: undefined,
+  volume: 3,
+  issue: 12,
+  pages: { from: 21, to: 37 },
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<{}>) {
   const userId = req.headers['user-id'] as string
   //? Should never happen
@@ -47,21 +72,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   const completion = formatChatAnswer(JSON.parse(answer)) as AnswerDto
 
+  const filteredCompletion = Object.keys(completion)
+    .filter((key) => key in exampleAnswer)
+    .reduce<AnswerDto>((obj, key) => {
+      obj[key as keyof AnswerDto] = completion[key as keyof AnswerDto] as any
+      return obj
+    }, {})
+
   //? Create paper in database
-  await prisma.paper.create({
-    data: {
-      ...completion,
-      userId,
-      type: completion.type === 'Journal Article' ? ReferenceType.ARTICLE : ReferenceType.BOOK,
-      authors: {
-        create: completion.authors?.map((author) => ({
-          fName: author.fName,
-          lName: author.lName,
-        })),
+  await prisma.paper
+    .create({
+      data: {
+        ...filteredCompletion,
+        userId,
+        type: completion.type === 'Journal Article' ? ReferenceType.ARTICLE : ReferenceType.BOOK,
+        authors: {
+          create: completion.authors?.map((author) => ({
+            fName: author.fName,
+            lName: author.lName,
+          })),
+        },
+        pages: completion.pages ? [completion.pages.from, completion.pages.to] : undefined,
       },
-      pages: completion.pages ? [completion.pages.from, completion.pages.to] : undefined,
-    },
-  })
+    })
+    .catch((error) => {
+      console.error(error)
+      return res.status(500).end()
+    })
 
   res.status(200).end()
 }
