89c58bc799092d5ef4607b26da460ed5454d2556
Mikołaj Jakóbiak <jakobiakmikolaj@gmail.com>
2023-05-01 23:06:34 +0100

    tweak

--- src/app/groups/Sidebar.tsx
+++ src/app/groups/Sidebar.tsx
@@ -10,7 +10,7 @@ export default async function Sidebar() {
   //? Should never happen
   if (!userId) return null
 
-  let groups = (await prisma.group
+  const groups = (await prisma.group
     .findMany({
       where: {
         userIds: {
